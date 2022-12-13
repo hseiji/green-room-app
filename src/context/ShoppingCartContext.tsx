16fed5fb138723d72328207586e49d09d70c9c4e
hseiji <henrique.hst@gmail.com>
2022-12-13 08:24:11 -0500

    update ShoppingCartContext

--- src/context/ShoppingCartContext.tsx
+++ src/context/ShoppingCartContext.tsx
@@ -1,16 +1,24 @@
-import { createContext, ReactNode, useContext } from 'react'
-
-const ShoppingCartContext = createContext({})
-
-export function useShoppingCart() {
-  return useContext(ShoppingCartContext)
-}
+import { createContext, ReactNode, useContext, useState } from 'react'
 
 type ShoppingCartProviderProps = {
   children: ReactNode
 }
 
+type ShoppingCartContext = {
+  getItemQuantity: (id: number) => number
+  increaseCartQuantity: (id: number) => void
+  decreaseCartQuantity: (id: number) => void
+  removeFromCart: (id: number) => void
+}
+
+const ShoppingCartContext = createContext({} as ShoppingCartContext)
+
+export function useShoppingCart() {
+  return useContext(ShoppingCartContext)
+}
+
 export function ShoppingCartProvider({ children }: ShoppingCartProviderProps) {
+  const [cartItems, setCartItems] = useState()
   return (
     <ShoppingCartContext.Provider value={{}}>
       {children}
