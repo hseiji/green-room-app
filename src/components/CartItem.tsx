6057fedcb102cf6152bac870374ce1c96f1df80b
hseiji <henrique.hst@gmail.com>
2022-12-22 20:13:41 -0500

    customize CartItem

--- src/components/CartItem.tsx
+++ src/components/CartItem.tsx
@@ -1,6 +1,7 @@
 import { Stack } from 'react-bootstrap'
 import { useShoppingCart } from '../context/ShoppingCartContext'
 import storeItems from '../data/items.json'
+import { formatCurrency } from '../helpers/formatCurrency'
 
 type CartItemProps = {
   id: number
@@ -12,11 +13,24 @@ export function CartItem({ id, quantity }: CartItemProps) {
   const item = storeItems.find((i) => i.id === id)
   if (item == null) return null
   return (
-    <Stack direction="horizontal" gap={2}>
+    <Stack direction="horizontal" gap={2} className="d-flex align-items-center">
       <img
         src={item.imgUrl}
         style={{ width: '125px', height: '75px', objectFit: 'cover' }}
       />
+      <div className="me-auto">
+        <div>
+          {item.name}{' '}
+          {quantity > 1 && (
+            <span className="text-muted" style={{ fontSize: '.65rem' }}>
+              x{quantity}
+            </span>
+          )}
+        </div>
+        <div className="text-muted" style={{ fontSize: '.75rem' }}>
+          {formatCurrency(item.price)}
+        </div>
+      </div>
     </Stack>
   )
 }
