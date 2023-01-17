410e8f82f6e86dbf73fac7a16eea0feca8473c27
hseiji <henrique.hst@gmail.com>
2023-01-16 22:00:45 -0500

    add more plants to selection

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -3,6 +3,7 @@ export function Home() {
     <>
       <h1>Home</h1>
       <p>Let's build our garden in our Green Room! =)</p>
+      <a href="plants">Build here!</a>
       <p>Insert instructions map over here...</p>
     </>
   )
