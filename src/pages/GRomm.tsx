36474ffb1135eb516518de500ba82d7be17164ac
hseiji <henrique.hst@gmail.com>
2023-01-17 08:17:28 -0500

    plan plants table

--- src/pages/GRomm.tsx
+++ src/pages/GRomm.tsx
@@ -1,3 +1,11 @@
 export function GRoom() {
-  return <h1>Green Room</h1>
+  return (
+    <>
+      <h1>Green Room</h1>
+      <p>
+        Add table here...import table from react-bootstrap
+        (https://react-bootstrap.netlify.app/components/table/)
+      </p>
+    </>
+  )
 }
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -5,6 +5,8 @@ export function Home() {
       <p>Let's build our garden in our Green Room! =)</p>
       <a href="plants">Build here!</a>
       <p>Insert instructions map over here...</p>
+      <p>Or if you already have your garden set up...</p>
+      <a href="groom">Go to Green Room!</a>
     </>
   )
 }
