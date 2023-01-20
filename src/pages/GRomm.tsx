fee03ee59151ac9ec998b30aad8fa5ea405059ae
hseiji <henrique.hst@gmail.com>
2023-01-19 21:39:27 -0500

    add table to GRoom

--- src/pages/GRomm.tsx
+++ src/pages/GRomm.tsx
@@ -1,11 +1,32 @@
+import { Table } from 'react-bootstrap'
+import storeItems from '../data/items.json'
+
 export function GRoom() {
   return (
     <>
       <h1>Green Room</h1>
-      <p>
-        Add table here...import table from react-bootstrap
-        (https://react-bootstrap.netlify.app/components/table/)
-      </p>
+      <h4>Plants Description</h4>
+      {storeItems.map((item) => {
+        item.name
+      })}
+
+      <Table>
+        <thead>
+          <th>Name</th>
+          <th>Desc</th>
+          <th>Water</th>
+          <th>Soil</th>
+          <th>Light</th>
+        </thead>
+        <tbody>
+          <td>test1a</td>
+          <td>test1b</td>
+          <td>test1c</td>
+          <td>test1d</td>
+          <td>test1e</td>
+        </tbody>
+      </Table>
+
       <p>2D room and place plants (numbers) in the space</p>
     </>
   )
