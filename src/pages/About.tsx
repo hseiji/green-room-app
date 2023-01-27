4b882b46b0477762fa0b4a4118a0007a8fdbf4f1
hseiji <henrique.hst@gmail.com>
2023-01-26 20:37:25 -0500

    update About page

--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -1,3 +1,13 @@
 export function About() {
-  return <h1>About</h1>
+  return (
+    <>
+      <h1>About</h1>
+      <h3>
+        The Green Room was created to give you a better understanding of your
+        green friends. Taking care of them sometimes it is not a simple task.
+        Add each plant to your list and then check detailed information such as
+        water, soil, light and more.
+      </h3>
+    </>
+  )
 }
