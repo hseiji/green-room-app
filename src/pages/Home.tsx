6aa70e0fa8ae3b0f33db8c8644bd0b52eead95b5
hseiji <henrique.hst@gmail.com>
2023-01-26 20:16:01 -0500

    update comments

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -4,8 +4,11 @@ export function Home() {
       <h1>Home</h1>
       <p>Let's build our garden in our Green Room! =)</p>
       <a href="plants">Build here!</a>
-      <p>Insert instructions map over here...</p>
-      <p>Or if you already have your garden set up...</p>
+      <p>
+        This app is still in progress. But you can play around adding and
+        removing plants to your room.
+      </p>
+      <p>Or if you already have your garden set up access again here below.</p>
       <a href="groom">Go to Green Room!</a>
     </>
   )
