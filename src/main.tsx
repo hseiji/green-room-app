be6fe646530384a13aa55be028e72dd16ec411b5
hseiji <henrique.hst@gmail.com>
2025-04-11 23:24:10 -0400

    set up node_env in vite.config

--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,13 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
-import "bootstrap/dist/css/bootstrap.min.css"
-import { BrowserRouter } from "react-router-dom"
+import 'bootstrap/dist/css/bootstrap.min.css'
+import { BrowserRouter } from 'react-router-dom'
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename="/green-room-app">
       <App />
     </BrowserRouter>
-  </React.StrictMode>
+  </React.StrictMode>,
 )
--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/green-room-app/',
+  // Use '/' for local dev and '/green-room-app/' for production
+  base: process.env.NODE_ENV === 'production' ? '/green-room-app/' : '/',
   plugins: [react()],
 })
